c4abd672aa1bccd4a7e2bbf07cc45a2b16c23f87
SayulRamirez <sayulramirez@gmail.com>
2024-11-26 23:18:04 -0600

    Implementando el patron singleton para la clase Conexion

--- src/Conexion.java
+++ src/Conexion.java
@@ -7,6 +7,47 @@
  *
  * @author sayul
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
     
+    private static Connection conexion;
+    
+    private Conexion() {
+        conectar();
+    }
+    
+    private static void conectar() {
+        try {
+            
+            conexion = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/biblioteca",
+                    "root",
+                    "Alej@ndro13"
+            );
+        } catch (SQLException e) {
+            System.out.println("Ocurrio un error: " + e.getErrorCode() + ", mensaje: " + e.getMessage());
+        }
+    }
+    
+    public static Connection obtenerConexion() {
+        if (conexion == null) {
+            new Conexion();
+        }
+        
+        return conexion;
+    }
+    
+    public static void cerrarConexion() {
+        if (conexion != null) {
+            try {
+                conexion.close();
+                conexion = null;
+            } catch (SQLException e) {
+                System.out.println("Ocurrio un error: " + e.getErrorCode() + ", mensaje: " + e.getMessage());
+            }
+        }
+    }
 }
